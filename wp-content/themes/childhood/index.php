065220612057bad66b9690402f9a3a9260defadb
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-03-18 19:22:32 +0300

    add about content

--- wp-content/themes/childhood/index.php
+++ wp-content/themes/childhood/index.php
@@ -89,19 +89,13 @@
     <div class="row">
       <div class="col-md-10 offset-md-1 col-lg-5 offset-lg-1">
         <div class="about__img">
-          <img src="<?= bloginfo(
-              'template_url'
-          ) ?>/assets/img/about.jpg" alt="про компанию">
+          <img src="<?php the_field('about_img'); ?>" alt="про компанию">
         </div>
       </div>
       <div class="col-md-10 offset-md-1 offset-lg-0 col-lg-6 col-xl-5 offset-xl-1">
-        <h1 class="title underlined">Про компанию</h1>
+        <h1 class="title underlined"><?php the_field('about_title'); ?></h1>
         <div class="about__text">
-          Наша компания уже больше десяти лет дарит позитивные эмоции детям и их родителям. Мы воплощаем все детские
-          мечты и помогаем родителям дарить счастливое детство!
-          <br> <br>
-          Но и взрослые иногда так нуждаются в детских эмоциях! Мы можем и это, ведь так приятно почувствовать заботу
-          даже когда тебе за... :)
+          <?php the_field('about_description'); ?>
         </div>
         <a href="#" class="button">Узнать больше</a>
       </div>
