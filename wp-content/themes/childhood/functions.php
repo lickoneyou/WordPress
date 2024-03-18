4717af574334770ba923f64d43a7d073613ca702
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-03-18 17:09:30 +0300

    add theme

---
+++ wp-content/themes/childhood/functions.php
@@ -0,0 +1,20 @@
+<?php
+add_action('wp_enqueue_scripts', 'childhood_scripts');
+add_action('wp_enqueue_scripts', 'childhood_styles');
+
+function childhood_scripts()
+{
+    wp_enqueue_script(
+        'childhood-scripts',
+        get_template_directory_uri() . '/assets/js/main.min.js',
+        [],
+        null,
+        true
+    );
+}
+
+function childhood_styles()
+{
+    wp_enqueue_style('childhood-style', get_stylesheet_uri());
+}
+?>
