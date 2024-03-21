1d13edb8c7c241d4d1aba12dd8253bfaa6fd5211
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-03-21 20:12:03 +0300

    add 404 page

---
+++ wp-content/themes/childhood/404.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+<div class='container toys'>
+  <h2 class='subtitle'>
+    <a href='<?= get_home_url(); ?>'>Такой Страницы не существует</a>
+  </h2>
+</div>
+
+
+<?php get_footer(); ?>
